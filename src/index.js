820b23cf4f1504593d58e1cbadb88cdc46394e50
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-26 01:50:08 +0530

    Made Change

--- src/index.js
+++ src/index.js
@@ -15,21 +15,36 @@ app.get("/findColleges", async (req, res) => {
   const city = req.query.city === undefined ? "" : req.query.city;
   const minPackage = Number(
     req.query.minPackage === undefined
-      ? 10.5
+      ? Number(0)
       : isNaN(req.query.minPackage)
-      ? 10.5
+      ? Number()
       : req.query.minPackage
   );
   const maxFees =
     req.query.maxFees === undefined
-      ? Number(10)
+      ? Number(1000)
       : isNaN(req.query.maxFees)
-      ? Number(10)
+      ? Number(1000)
       : Number(req.query.maxFees);
+
   const course = req.query.course === undefined ? "" : req.query.course;
   const exam = req.query.exam === undefined ? "" : req.query.exam;
-  console.log(Object.keys(req.query).length);
-  if (Object.keys(req.query).length !== 0) {
+  console.log(minPackage === 0);
+  if (
+    Object.keys(req.query).length !== 0 &&
+    minPackage === 0 &&
+    maxFees === 0
+  ) {
+    data = await connection.find({
+      name: { $regex: `${name}`, $options: "i" },
+      state: { $regex: `${state}`, $options: "i" },
+      city: { $regex: `${city}`, $options: "i" },
+
+      course: { $regex: `${course}`, $options: "i" },
+      exam: { $regex: `${exam}`, $options: "i" },
+    });
+    console.log("if");
+  } else if (Object.keys(req.query).length !== 0) {
     data = await connection.find({
       name: { $regex: `${name}`, $options: "i" },
       state: { $regex: `${state}`, $options: "i" },
@@ -39,17 +54,10 @@ app.get("/findColleges", async (req, res) => {
       course: { $regex: `${course}`, $options: "i" },
       exam: { $regex: `${exam}`, $options: "i" },
     });
-    console.log("if");
   } else {
     data = await connection.find();
   }
   res.send(data);
-  //   console.log("hwllo");
-  // } else {
-  //   data = await connection.find();
-  // }
-  // res.send(data);
-  // console.log(data);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
